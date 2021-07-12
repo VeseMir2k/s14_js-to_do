bc3916aea9a87fa1e3be64269e7d2e5d3d89cf12
Adam Stępień <adamstepien.kontakt@gmail.com>
2021-07-12 18:42:34 +0200

    download buttons and input

---
+++ main.js
@@ -0,0 +1,4 @@
+const addTaskModalBtn = document.querySelector(".add-task-modal-btn");
+const addTaskBtn = document.querySelector(".add-task-btn");
+const exitModalBtn = document.querySelector(".exit-modal-btn");
+const addTaskInput = document.querySelector(".add-task-modal input");
