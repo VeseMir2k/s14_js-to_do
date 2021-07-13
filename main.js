751b65d95f2182ba5d9d86a207c220584979d518
Adam Stępień <adamstepien.kontakt@gmail.com>
2021-07-13 17:04:34 +0200

    add addTask and tasks array

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 const openModalBtn = document.querySelector(".open-modal-btn");
 const exitModalBtn = document.querySelector(".exit-modal-btn");
 const addTaskBtn = document.querySelector(".add-task-btn");
-const addTaskInput = document.querySelector(".add-task-modal input");
+const taskNameInput = document.querySelector(".task-name-input");
 
 const blurModal = document.querySelector(".blur-modal");
 const addTaskModal = document.querySelector(".add-task-modal");
 
+const tasks = [
+  {
+    name: "Zadanie 1",
+  },
+  {
+    name: "Zadanie 2",
+  },
+  {
+    name: "Zadanie 3",
+  },
+];
+
 const openModal = () => {
   blurModal.style.zIndex = 1;
   addTaskModal.style.display = "flex";
+  taskNameInput.value = "";
+  document.querySelector(".error-name").textContent = "";
 };
 
 const exitModal = () => {
@@ -16,5 +30,17 @@ const exitModal = () => {
   addTaskModal.style.display = "none";
 };
 
+const addTask = () => {
+  event.preventDefault();
+  name = taskNameInput.value;
+  if (name.length >= 3) {
+    tasks.push({ name: name });
+    exitModal();
+  } else {
+    document.querySelector(".error-name").textContent = "Wpisz więcej znaków!";
+  }
+};
+
 openModalBtn.addEventListener("click", openModal);
 exitModalBtn.addEventListener("click", exitModal);
+addTaskBtn.addEventListener("click", addTask);
